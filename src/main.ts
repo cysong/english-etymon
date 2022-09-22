1f8171f07b69e8639ea556b7253039051f5d9ce4
yansong <yansongc@gmail.com>
2022-09-22 09:52:29 +0800

    integrate naive-ui

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,18 @@ const app = createApp(App)
 
 app.use(createPinia())
 
+
+import {
+    // create naive ui
+    create,
+    // component
+    NButton,
+    NConfigProvider
+} from 'naive-ui'
+
+const naive = create({
+    components: [NButton]
+})
+app.use(naive)
+
 app.mount('#app')
